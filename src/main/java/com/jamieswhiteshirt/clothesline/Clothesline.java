518b207e5886a83020fabdab0811eee28dbd020e
Erlend Åmdal <erlend@aamdal.com>
2017-09-29 11:28:29 +0200

    initial commit

---
+++ src/main/java/com/jamieswhiteshirt/clothesline/Clothesline.java
@@ -0,0 +1,15 @@
+package com.jamieswhiteshirt.clothesline;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Clothesline.MODID, version = Clothesline.VERSION)
+public class Clothesline {
+    public static final String MODID = "clothesline";
+    public static final String VERSION = "0.0.0.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+    }
+}
