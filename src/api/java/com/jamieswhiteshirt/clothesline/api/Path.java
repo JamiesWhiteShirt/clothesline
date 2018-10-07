8e6dc9ac4d4665194ed82428b32807dcc5cfc4b8
Erlend Åmdal <erlend@aamdal.com>
2018-10-07 18:06:49 +0200

    Fix the same error elsewhere

--- src/api/java/com/jamieswhiteshirt/clothesline/api/Path.java
+++ src/api/java/com/jamieswhiteshirt/clothesline/api/Path.java
@@ -37,9 +37,9 @@ public final class Path {
             if (minIndex != maxIndex) {
                 int middleIndex = (minIndex + maxIndex) / 2;
                 int comparison = DeltaComparator.getInstance().compare(delta, edges.get(middleIndex).getDelta());
-                if (comparison < 0) {
+                if (comparison > 0) {
                     return floorDeltaIndex(delta, minIndex, middleIndex);
-                } else if (comparison > 0) {
+                } else if (comparison < 0) {
                     return floorDeltaIndex(delta, middleIndex + 1, maxIndex);
                 } else {
                     return middleIndex;
